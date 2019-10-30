7f07feb07be47c6047878f354e7da02053da9d5d
matteonoccodev <52407199+matteonoccodev@users.noreply.github.com>
2019-10-30 09:47:01 +0100

    Creati servizi ICreateNewService e IGetTemplateToCreateNewService.

---
+++ src/backend/DomainModel/Services/ICreateNewService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.Services
+{
+    public interface ICreateNewService
+    {
+        void Insert(string publicToken, string privateToken, string welcomeMessage);
+    }
+}
---
+++ src/backend/DomainModel/Services/IGetTemplateToCreateNewService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.Services
+{
+    public interface IGetTemplateToCreateNewService
+    {
+        void Get();
+    }
+}
