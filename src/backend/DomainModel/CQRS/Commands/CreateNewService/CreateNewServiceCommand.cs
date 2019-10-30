dde4cef9afc552c84db5b73321c8089c917ca28b
matteonoccodev <52407199+matteonoccodev@users.noreply.github.com>
2019-10-30 09:54:53 +0100

    Creato Command di CreateNewService.

---
+++ src/backend/DomainModel/CQRS/Commands/CreateNewService/CreateNewServiceCommand.cs
@@ -0,0 +1,12 @@
+﻿using DomainModel.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.CQRS.Commands.CreateNewService
+{
+    public class CreateNewServiceCommand
+    {
+        public Service Service { get; set; }
+    }
+}
---
+++ src/backend/DomainModel/CQRS/Commands/CreateNewService/CreateNewServiceCommandHadler.cs
@@ -0,0 +1,20 @@
+﻿using CQRS.Commands;
+using DomainModel.Services;
+
+namespace DomainModel.CQRS.Commands.CreateNewService
+{
+    public class CreateNewServiceCommandHandler : ICommandHandler<CreateNewServiceCommand>
+    {
+        private readonly ICreateNewService createNewService;
+
+        public CreateNewServiceCommandHandler(ICreateNewService createNewService)
+        {
+            this.createNewService = createNewService;
+        }
+
+        public void Handle(CreateNewServiceCommand createNewService)
+        {
+            this.createNewService.Insert(createNewService.PublicToken, createNewService.PublicToken, createNewService.WelcomeMessage);
+        }
+    }
+}
