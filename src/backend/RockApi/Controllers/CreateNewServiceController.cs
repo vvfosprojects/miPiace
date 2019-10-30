6e1d394713a6bf3f7afc504c14b3c5de08fdf61b
matteonoccodev <52407199+matteonoccodev@users.noreply.github.com>
2019-10-30 10:01:29 +0100

    Creto il controller CreateNewServiceController.

---
+++ src/backend/RockApi/Controllers/CreateNewServiceController.cs
@@ -0,0 +1,48 @@
+﻿using CQRS.Commands;
+using DomainModel.Classes;
+using DomainModel.CQRS.Commands.CreateNewService;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MiPiace.Controllers
+{
+    public class CreateNewServiceController : Controller
+    {
+
+        private readonly ICommandHandler<CreateNewServiceCommand> handler;
+
+        public CreateNewServiceController(ICommandHandler<CreateNewServiceCommand> handler)
+        {
+            this.handler = handler;
+        }
+
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpPost]
+        public ActionResult Insert([FromBody] string welcomeMessage)
+        {
+            var service = new CreateNewServiceCommand()
+            {
+                Service = new Service()
+                {
+                    Id = new Guid(),
+                    PrivateToken = "",
+                    PublicToken = "",
+                    WelcomeMessage = welcomeMessage
+                }
+            };
+            handler.Handle(service);
+
+            return Ok();
+        }
+    }
+}
+
+    
