3ac50ac38f09a8505f24756b4b5ea320616e5437
matteonoccodev <52407199+matteonoccodev@users.noreply.github.com>
2019-10-30 09:45:30 +0100

    Creata la classe Service.

---
+++ src/backend/DomainModel/Classes/Service.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainModel.Classes
+{
+    public class Service
+    {
+        //Rappresenta l'ID Mongo del servizio
+        public Guid Id { get; set; }
+
+        //Rappresenta l'identificativo che verrà esposto al pubblico per richiamare il servizio 
+        public string PublicToken { get; set; }
+
+        //Rappresenta l'identificativo che verrà fornito al proprietario del servizio, che utilizzerà per accedere alla statistiche relative al servizio 
+        public string PrivateToken { get; set; }
+
+        //Rappresenta il messaggio che verrà mostrato all'utente in fase di rilascio del feedback
+        public string WelcomeMessage { get; set; }
+    }
+}
