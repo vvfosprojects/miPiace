c61ecc7050da9b4f3f54657e1cc9efc4cdb266ad
matteonoccodev <52407199+matteonoccodev@users.noreply.github.com>
2019-10-30 11:48:24 +0100

    Implementato il metodo Insert nel controller. Creato Helper GenerateToken per la generazione randomica di stringhe contenenti 32 caratteri.

--- src/backend/RockApi/Controllers/CreateNewServiceController.cs
+++ src/backend/RockApi/Controllers/CreateNewServiceController.cs
@@ -2,13 +2,15 @@
 using DomainModel.Classes;
 using DomainModel.CQRS.Commands.CreateNewService;
 using Microsoft.AspNetCore.Mvc;
+using MiPiace.Helper;
 using System;
 
 namespace MiPiace.Controllers
 {
-    public class CreateNewServiceController : Controller
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CreateNewServiceController : ControllerBase
     {
-
         private readonly ICommandHandler<CreateNewServiceCommand> handler;
 
         public CreateNewServiceController(ICommandHandler<CreateNewServiceCommand> handler)
@@ -16,15 +18,19 @@ namespace MiPiace.Controllers
             this.handler = handler;
         }
 
-
         [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return Ok();
         }
 
-
-
+        /// <summary>
+        /// Il metodo Insert permette la creazione di un nuovo servizio con relativo welcomeMessage. Questo servizio verrà utilizzato
+        /// una tantum, nel momento della registrazione dello stesso.
+        /// </summary>
+        /// <param name="welcomeMessage">Rappresenta il messaggio che verrà visualizzato nella pagina mostrata all'utente che vuole rilasciare 
+        /// il feedback.</param>
+        /// <returns>200 Ok, se l'operazione è stata eseguita correttamente.</returns>
         [HttpPost]
         public ActionResult Insert([FromBody] string welcomeMessage)
         {
@@ -33,14 +39,21 @@ namespace MiPiace.Controllers
                 Service = new Service()
                 {
                     Id = new Guid(),
-                    PrivateToken = "",
-                    PublicToken = "",
-                    WelcomeMessage = welcomeMessage
+                    PrivateToken = GenerateToken.generate(32),
+                    PublicToken = GenerateToken.generate(32),
+                    WelcomeMessage = welcomeMessage.ToString()
                 }
             };
-            handler.Handle(service);
 
-            return Ok();
+            try
+            {
+                handler.Handle(service);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
         }
     }
 }
---
+++ src/backend/RockApi/Helper/GenerateToken.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MiPiace.Helper
+{
+    public class GenerateToken
+    {
+        public static string generate(int length)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 1; i < length; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+
+            return builder.ToString();
+        }
+    }
+}
