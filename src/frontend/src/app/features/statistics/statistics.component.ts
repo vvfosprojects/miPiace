b8256ce96dee1b2d006a5bec51d61ce17d416693
Davide Cappa <cappadavide@gmail.com>
2019-11-19 09:54:34 +0100

    Aggiunto average Statistic all'onInit del componente

--- src/frontend/src/app/features/statistics/statistics.component.ts
+++ src/frontend/src/app/features/statistics/statistics.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ManageFeedbackService } from '../../core/services/manage-feedback.service';
+import { FeedbackScore } from '../../shared/models/feedback-score';
 
 @Component({
   selector: 'app-statistics',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./statistics.component.css']
 })
 export class StatisticsComponent implements OnInit {
+  private privateToken: string;
 
-  constructor() { }
+  constructor(private manageFeedbackService: ManageFeedbackService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit() {
+    this.privateToken = this.route.snapshot.paramMap.get('id');
+
+    this.manageFeedbackService
+      .getServiceAverageFeedbackScore(this.privateToken)
+      .subscribe((feedbackScore: FeedbackScore) => console.log(feedbackScore));
   }
 
 }
